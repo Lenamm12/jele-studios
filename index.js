88a9b1572f2b2be72954336a42a0100fc2f207f6
Lena Morawietz <45099435+Lenamm12@users.noreply.github.com>
2026-03-07 20:55:41 +0100

    details dialog with content

---
+++ index.js
@@ -0,0 +1,8 @@
+// show dialog with details when card btn is clicked
+// const cards = document.querySelectorAll(".card");
+
+// cards.forEach((card) => {
+//     card.querySelector("details").addEventListener("click", () => {
+//         card.querySelector("dialog").classList.toggle("show");
+//     });
+// });
